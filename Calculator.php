2b363d884890093adf75db5b3d4f9eaa24e7a498
mfaraji <mfaraji@mfarajis-MacBook-Pro.local>
2019-10-21 23:09:39 -0700

    fixes

--- Calculator.php
+++ Calculator.php
@@ -2,6 +2,10 @@
 
 class Calculator
 {
+    function __construct() {
+        print "In BaseClass constructor\n";
+    }
+     
     public function add($arg1, $arg2)
     {
         return $arg1 + $arg2;
